a67bff4c8ba2448c914475a6f6c9e486d5d5153a
Lorenz Schüler <lorenz@saarow.de>
2025-11-05 14:23:47 +0100

    Add DiskRepresentable trait (#161)

--- rust/src/storage/file/node_file_storage/mod.rs
+++ rust/src/storage/file/node_file_storage/mod.rs
@@ -18,14 +18,13 @@ use std::{
     },
 };
 
-use zerocopy::{FromBytes, Immutable, IntoBytes};
-
 use crate::{
     error::BTResult,
     storage::{
         CheckpointParticipant, Error, Storage,
         file::{FileBackend, FromToFile},
     },
+    types::DiskRepresentable,
 };
 
 mod node_file_storage_metadata;
@@ -62,7 +61,7 @@ impl<T, F> NodeFileStorage<T, F> {
 
 impl<T, F> Storage for NodeFileStorage<T, F>
 where
-    T: FromBytes + IntoBytes + Immutable + Send + Sync,
+    T: DiskRepresentable + Send + Sync,
     F: FileBackend,
 {
     type Id = u64;
@@ -118,15 +117,13 @@ where
     }
 
     fn get(&self, idx: Self::Id) -> BTResult<Self::Item, Error> {
-        let offset = idx * size_of::<Self::Item>() as u64;
-        if self.node_file.len()? < offset + size_of::<T>() as u64
+        let offset = idx * T::size() as u64;
+        if self.node_file.len()? < offset + T::size() as u64
             || self.reuse_list_file.lock().unwrap().contains(idx)
         {
             return Err(Error::NotFound.into());
         }
-        // this is hopefully optimized away
-        let mut node = T::new_zeroed();
-        self.node_file.read_exact_at(node.as_mut_bytes(), offset)?;
+        let node = T::from_disk_repr(|buf| self.node_file.read_exact_at(buf, offset))?;
         Ok(node)
     }
 
@@ -146,8 +143,9 @@ where
         } else if idx < self.metadata.read().unwrap().frozen_nodes {
             return Err(Error::Frozen.into());
         }
-        let offset = idx * size_of::<Self::Item>() as u64;
-        self.node_file.write_all_at(node.as_bytes(), offset)?;
+        let offset = idx * T::size() as u64;
+        self.node_file
+            .write_all_at(node.to_disk_repr().as_ref(), offset)?;
         Ok(())
     }
 
@@ -235,6 +233,8 @@ mod tests {
         fs::{self, File},
         io::Read,
     };
+
+    use zerocopy::IntoBytes;
 
     use super::*;
     use crate::{
@@ -814,7 +814,7 @@ mod tests {
 
     impl<T, F> super::NodeFileStorage<T, F>
     where
-        T: IntoBytes + Immutable,
+        T: DiskRepresentable,
     {
         /// Creates all files for a file-based node storage in the specified directory
         /// and populates them with the provided nodes and reusable indices.
---
+++ rust/src/types/disk_representable.rs
@@ -0,0 +1,86 @@
+// Copyright (c) 2025 Sonic Operations Ltd
+//
+// Use of this software is governed by the Business Source License included
+// in the LICENSE file and at soniclabs.com/bsl11.
+//
+// Change Date: 2028-4-16
+//
+// On the date above, in accordance with the Business Source License, use of
+// this software will be governed by the GNU Lesser General Public License v3.
+
+use std::{borrow::Cow, mem};
+
+use zerocopy::{FromBytes, Immutable, IntoBytes};
+
+/// A trait for types that can be represented as raw bytes on disk.
+/// The disk representation does not need to be the same as the in-memory representation.
+/// There is a blanket implementation for types implementing [`FromBytes`], [`IntoBytes`] and
+/// [`Immutable`].
+pub trait DiskRepresentable {
+    /// Constructs the value from its disk representation. `read_into_buffer` is expected to fill
+    /// the provided buffer with the raw bytes read from disk. It is up to the implementation to
+    /// convert the buffer into the value.
+    fn from_disk_repr<E>(
+        read_into_buffer: impl FnOnce(&mut [u8]) -> Result<(), E>,
+    ) -> Result<Self, E>
+    where
+        Self: Sized;
+
+    /// Returns the disk representation of the value as a byte slice (borrowed from self or owned
+    /// depending on whether the disk representations is the same as the in-memory representation or
+    /// needed to be serialized).
+    fn to_disk_repr(&'_ self) -> Cow<'_, [u8]>;
+
+    /// Returns the size of the disk representation in bytes.
+    fn size() -> usize;
+}
+
+impl<T: FromBytes + IntoBytes + Immutable> DiskRepresentable for T {
+    fn from_disk_repr<E>(
+        read_into_buffer: impl FnOnce(&mut [u8]) -> Result<(), E>,
+    ) -> Result<Self, E> {
+        let mut value = T::new_zeroed();
+        read_into_buffer(value.as_mut_bytes())?;
+        Ok(value)
+    }
+
+    fn to_disk_repr(&'_ self) -> Cow<'_, [u8]> {
+        Cow::Borrowed(self.as_bytes())
+    }
+
+    fn size() -> usize {
+        mem::size_of::<Self>()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Debug, PartialEq, Eq, FromBytes, IntoBytes, Immutable)]
+    struct ZerocopyableType([u8; 32]);
+
+    #[test]
+    fn from_disk_repr_for_zerocopy_types_calls_read_into_buffer_with_bytes_representation_and_returns_reinterpreted_data()
+     {
+        let value = ZerocopyableType::from_disk_repr(|buf| {
+            buf.fill(1u8);
+            Ok::<_, ()>(())
+        })
+        .unwrap();
+
+        assert_eq!(value, ZerocopyableType([1u8; 32]));
+    }
+
+    #[test]
+    fn to_disk_repr_for_zerocopy_types_returns_borrowed_bytes_representation() {
+        let value = ZerocopyableType([1; 32]);
+        let disk_repr = value.to_disk_repr();
+        assert_eq!(disk_repr, Cow::Borrowed(&[1u8; 32]));
+    }
+
+    #[test]
+    fn size_for_zerocopy_types_returns_size_of_type() {
+        assert_eq!(ZerocopyableType::size(), 32);
+    }
+}
