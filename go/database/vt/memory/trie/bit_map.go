4601c9e136da4cbca5c61061bbbddc1dd4cac7be
Herbert <herbert@swrnd.ch>
2025-11-14 10:59:18 +0100

    Add bitmap utility for go-memory implementation (#208)

---
+++ go/database/vt/memory/trie/bit_map.go
@@ -0,0 +1,37 @@
+// Copyright (c) 2025 Sonic Operations Ltd
+//
+// Use of this software is governed by the Business Source License included
+// in the LICENSE file and at soniclabs.com/bsl11.
+//
+// Change Date: 2028-4-16
+//
+// On the date above, in accordance with the Business Source License, use of
+// this software will be governed by the GNU Lesser General Public License v3.
+
+package trie
+
+// bitMap is a simple bitmap implementation for 256 bits.
+type bitMap [256 / 64]uint64
+
+// get returns true if the bit at the specified index is set.
+func (b *bitMap) get(index byte) bool {
+	return (b[index/64] & (1 << (index % 64))) != 0
+}
+
+// set sets the bit at the specified index.
+func (b *bitMap) set(index byte) {
+	b[index/64] |= 1 << (index % 64)
+}
+
+// clear clears all bits in the bitmap.
+func (b *bitMap) clear() {
+	b[0] = 0
+	b[1] = 0
+	b[2] = 0
+	b[3] = 0
+}
+
+// any returns true if any bit in the bitmap is set.
+func (b *bitMap) any() bool {
+	return b[0]|b[1]|b[2]|b[3] != 0
+}
---
+++ go/database/vt/memory/trie/bit_map_test.go
@@ -0,0 +1,74 @@
+// Copyright (c) 2025 Sonic Operations Ltd
+//
+// Use of this software is governed by the Business Source License included
+// in the LICENSE file and at soniclabs.com/bsl11.
+//
+// Change Date: 2028-4-16
+//
+// On the date above, in accordance with the Business Source License, use of
+// this software will be governed by the GNU Lesser General Public License v3.
+
+package trie
+
+import (
+	"slices"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestBitMap_SetAndGet(t *testing.T) {
+	require := require.New(t)
+	indices := []byte{0, 1, 2, 3, 4, 5, 63, 64, 127, 128, 191, 192, 255}
+
+	var bm bitMap
+	for i, index := range indices {
+		for j := range indices {
+			require.Equal(j < i, bm.get(indices[j]), "Before setting: i=%d,j=%d", i, j)
+		}
+		bm.set(index)
+		for j := range indices {
+			require.Equal(j <= i, bm.get(indices[j]), "After setting: i=%d,j=%d", i, j)
+		}
+	}
+}
+
+func TestBitMap_Any_TrueIfAnyFlagIsSet(t *testing.T) {
+	require := require.New(t)
+	indices := []byte{0, 1, 2, 3, 4, 5, 63, 64, 127, 128, 191, 192, 255}
+
+	for _, index := range indices {
+		var bm bitMap
+		require.False(bm.any(), "Bitmap should be empty initially")
+		bm.set(index)
+		require.True(bm.any(), "Bitmap should report non-empty after setting index %d", index)
+	}
+
+	// also multiple bits are fine
+	var bm bitMap
+	bm.set(1)
+	bm.set(128)
+	require.True(bm.any())
+}
+
+func TestBitMap_Clear_RemovesAllBits(t *testing.T) {
+	require := require.New(t)
+	indices := []byte{0, 1, 2, 3, 4, 5, 63, 64, 127, 128, 191, 192, 255}
+
+	var bm bitMap
+	for _, index := range indices {
+		bm.set(index)
+	}
+	for i := range 256 {
+		require.Equal(
+			slices.Contains(indices, byte(i)),
+			bm.get(byte(i)),
+		)
+	}
+
+	bm.clear()
+	require.False(bm.any(), "Bitmap should be empty after clear")
+	for i := range 256 {
+		require.False(bm.get(byte(i)), "All bits should be cleared after clear")
+	}
+}
